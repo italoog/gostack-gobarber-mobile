1a3c0f3b49d8df1aa24ad69112815768e73904c8
Ítalo Gomes <47856024+italoog@users.noreply.github.com>
2021-02-19 16:53:28 -0300

    27.00

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { TextInputProps } from 'react-native';
+import { useField } from '@unform/core';
 
 import { Container, TextInput, Icon } from './styles';
 
@@ -8,16 +9,27 @@ interface InputProps extends TextInputProps {
   icon: string;
 }
 
-const Input: React.FC<InputProps> = ({ name, icon, ...rest }) => (
-  <Container>
-    <Icon name={icon} size={20} color="#666360" />
+const Input: React.FC<InputProps> = ({ name, icon, ...rest }) => {
+  const inputRef = useRef();
+  const { registerField, defaultValue, fieldName, error } = useField(name);
 
-    <TextInput
-      keyboardAppearance="dark"
-      placeholderTextColor="#666360"
-      {...rest}
-    />
-  </Container>
-);
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+    });
+  }, []);
+
+  return (
+    <Container>
+      <Icon name={icon} size={20} color="#666360" />
+
+      <TextInput
+        keyboardAppearance="dark"
+        placeholderTextColor="#666360"
+        {...rest}
+      />
+    </Container>
+  );
+};
 
 export default Input;
